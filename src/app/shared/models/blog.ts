3e83b2452281e4886819b70990f669758a8f67a5
Kevin Norton <rhchristian1@gmail.com>
2018-01-02 06:42:30 +0000

    shared (models): added models

--- src/app/shared/models/blog.ts
+++ src/app/shared/models/blog.ts
@@ -1,2 +1,9 @@
 export class Blog {
+    id: Number;
+    title: String;
+    author: String;
+    publish_date: String; 
+    slug: String;       
+    description: String;  
+    content: String;      
 }
--- src/app/shared/models/comment.ts
+++ src/app/shared/models/comment.ts
@@ -1,2 +1,8 @@
 export class Comment {
+  id: Number;
+  postId: Number;
+  parent_id: Number | null;
+  user: String;
+  date: String;
+  content: String;
 }
