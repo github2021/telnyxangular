16345048c698b9fde784aaba7313145dff77ff49
Kevin Norton <rhchristian1@gmail.com>
2018-01-02 02:22:51 +0000

    pages (blog): added functionality to get blog from service

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Observable } from 'rxjs/Observable'
+
+import { RestfulApiService } from '../../restful-api.service'
+import { Blog } from '../../shared/models/blog'
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  blogId: number
+  blog$: Observable < Blog >
+
+    constructor(
+      private title: Title,
+      private activatedRoute: ActivatedRoute,
+      private router: Router,
+      private restfulAPIService: RestfulApiService
+    ) {}
 
   ngOnInit() {
+    this.title.setTitle("Blog")
+
+    this.activatedRoute.paramMap.subscribe(params => {
+      if (params.has('id')) {
+        this.blogId = Number(params.get('id'))
+        this.getBlog()
+      }
+    })
+
+  }
+
+
+  getBlog() {
+
+    this.blog$ = this.restfulAPIService.getBlog(this.blogId)
   }
 
 }
