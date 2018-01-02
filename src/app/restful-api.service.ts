436157899bdab65eab4cb070497cca26a5064a61
Kevin Norton <rhchristian1@gmail.com>
2018-01-02 01:30:27 +0000

    service (restapi): functionality to get blogs sorted by newest from api

--- src/app/restful-api.service.ts
+++ src/app/restful-api.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+import { Blog } from './shared/models/blog'
+
+const API_URL = "http://localhost:9001"
 
 @Injectable()
 export class RestfulApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+
+  private sortBlogsByDate(a, b) {
+    return new Date(b.publish_date).getTime() - new Date(a.publish_date).getTime();
+  }
+
+  public getBlogs() {
+    return this.http.get < Blog[] > (`${API_URL}/posts`)
+      .pipe(map(x => x.sort(this.sortBlogsByDate)));
+
+  }
 
 }
